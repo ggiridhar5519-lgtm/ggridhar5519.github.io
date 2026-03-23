43f775688361540b0bc3ae5ac1c4d8674839f9b1
Service.in <ggiridhar5519@gmail.com>
2026-03-23 10:36:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+// ACTIVE NAV LINK
 const links = document.querySelectorAll(".nav-link");
 const currentPage = location.pathname.split("/").pop();
 
 links.forEach(link => {
-  if (link.getAttribute("href") === currentPage) {
-    link.classList.add("active");
-  }
+    if (link.getAttribute("href") === currentPage) {
+        link.classList.add("active");
+    }
 });
+
+// HAMBURGER MENU TOGGLE
+function toggleMenu() {
+    const nav = document.getElementById("navLinks");
+    nav.classList.toggle("active");
+}
