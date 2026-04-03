e40146766cc5fb4374e49cc6a9f3651ac642cb6e
Service.in <ggiridhar5519@gmail.com>
2026-04-03 09:34:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,3 @@ toggle.addEventListener("click", () => {
   toggle.classList.toggle("active");
   menu.classList.toggle("show");
 });
-
-/* CLOSE WHEN CLICK OUTSIDE */
-document.addEventListener("click", (e) => {
-  if (!toggle.contains(e.target) && !menu.contains(e.target)) {
-    toggle.classList.remove("active");
-    menu.classList.remove("show");
-  }
-});
