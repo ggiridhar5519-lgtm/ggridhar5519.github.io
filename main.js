2863a0d65639d5d6cf829d50e89253fadb90e8a5
Service.in <ggiridhar5519@gmail.com>
2026-04-03 11:14:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
 });
+// SCROLL REVEAL
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  const trigger = window.innerHeight * 0.85;
+
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < trigger) {
+      el.classList.add("active");
+    }
+  });
+});
