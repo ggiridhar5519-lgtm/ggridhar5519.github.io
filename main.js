a80edf90daeaff1e1b6a3086943c27ca36474af0
Service.in <ggiridhar5519@gmail.com>
2026-04-03 11:43:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,40 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const intro = document.getElementById("intro");
   const toggle = document.getElementById("menuToggle");
   const menu = document.getElementById("mobileMenu");
-  const navbar = document.querySelector(".navbar");
 
-  // INTRO
   setTimeout(() => {
     intro.classList.add("hide");
-  }, 3000);
+  }, 2500);
 
-  // MOBILE MENU
-  if (toggle && menu) {
-    toggle.addEventListener("click", () => {
-      menu.classList.toggle("show");
-    });
-  }
-
-  // SCROLL NAVBAR
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-      navbar.classList.add("scrolled");
-    } else {
-      navbar.classList.remove("scrolled");
-    }
-  });
-
-  // SCROLL ANIMATION
-  const reveals = document.querySelectorAll(".reveal");
-
-  window.addEventListener("scroll", () => {
-    const trigger = window.innerHeight * 0.85;
-
-    reveals.forEach(el => {
-      if (el.getBoundingClientRect().top < trigger) {
-        el.classList.add("active");
-      }
-    });
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("show");
   });
 
 });
