99ed9c5516c286149a8f3854ab6f08378ff0cb00
Service.in <ggiridhar5519@gmail.com>
2026-04-03 12:43:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const intro = document.getElementById("intro");
+  const progress = document.querySelector(".progress");
   const status = document.getElementById("statusDot");
 
-  // INTRO EXIT
+  // INTRO
   setTimeout(() => {
     intro.classList.add("hide");
   }, 2500);
 
-  // ONLINE STATUS
+  // STATUS
   const hour = new Date().getHours();
   status.style.background = (hour >= 9 && hour <= 18) ? "limegreen" : "gray";
+
+  // SCROLL BAR
+  window.addEventListener("scroll", () => {
+    const scrolled = window.scrollY /
+      (document.body.scrollHeight - window.innerHeight);
+    progress.style.width = scrolled * 100 + "%";
+  });
 
 });
