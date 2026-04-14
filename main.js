ee8ae32752666b8b36223968e4f6cd717437eea6
Service.in <ggiridhar5519@gmail.com>
2026-04-14 11:44:19 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,19 +2,44 @@
 window.onload = () => {
   setTimeout(() => {
     document.getElementById("intro").classList.add("hide");
-  }, 1500);
+  }, 1200);
 };
 
-// SCROLL ANIMATION
-const revealElements = document.querySelectorAll(".reveal");
-
+// SCROLL
+const reveal = document.querySelectorAll(".reveal");
 window.addEventListener("scroll", () => {
-  revealElements.forEach(el => {
-    const windowHeight = window.innerHeight;
-    const elementTop = el.getBoundingClientRect().top;
-
-    if (elementTop < windowHeight - 100) {
+  reveal.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
       el.classList.add("active");
     }
   });
 });
+
+// TYPING EFFECT
+const text = ["Engineer","Developer","Architect"];
+let i = 0, j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+  current = text[i];
+  
+  if (!isDeleting) {
+    document.getElementById("typing").textContent = current.substring(0, j++);
+    if (j > current.length) {
+      isDeleting = true;
+      setTimeout(type, 1000);
+      return;
+    }
+  } else {
+    document.getElementById("typing").textContent = current.substring(0, j--);
+    if (j < 0) {
+      isDeleting = false;
+      i = (i + 1) % text.length;
+    }
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
