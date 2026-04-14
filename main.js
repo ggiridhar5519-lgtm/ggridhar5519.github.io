b63b79f7f60df3f72bcc91015b85190e3a090538
Service.in <ggiridhar5519@gmail.com>
2026-04-14 10:49:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const intro = document.getElementById("intro");
-  const toggle = document.getElementById("menuToggle");
-  const menu = document.getElementById("mobileMenu");
-  const close = document.getElementById("closeMenu");
   const status = document.getElementById("statusDot");
 
-  /* INTRO */
   setTimeout(() => {
     intro.classList.add("hide");
-  }, 2200);
+  }, 1500);
 
-  /* MENU */
-  toggle.addEventListener("click", () => {
-    menu.classList.add("show");
-  });
-
-  close.addEventListener("click", () => {
-    menu.classList.remove("show");
-  });
-
-  /* STATUS */
   const hour = new Date().getHours();
   status.style.background = (hour >= 9 && hour <= 18)
-    ? "limegreen"
+    ? "green"
     : "gray";
 
 });
