b81e3e729240f16d1efe963a2e2b7d8aa88d9b07
Service.in <ggiridhar5519@gmail.com>
2026-04-16 11:18:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 window.addEventListener("load", () => {
   const intro = document.getElementById("intro");
 
-  if (intro) {
-    setTimeout(() => {
-      intro.classList.add("hide");
-    }, 2500);
-  }
+  setTimeout(() => {
+    intro.classList.add("hide");
+  }, 2500);
 });
 
-// MOBILE MENU
-const menu = document.getElementById("mobileMenu");
-const toggleBtn = document.getElementById("menuToggle");
-const closeBtn = document.getElementById("closeMenu");
-
-toggleBtn.onclick = () => menu.classList.add("show");
-closeBtn.onclick = () => menu.classList.remove("show");
+function toggleMenu() {
+  document.getElementById("mobileMenu").classList.toggle("show");
+}
